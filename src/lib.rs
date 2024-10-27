570059f86b0bd034a92c0640b74435331e1b20fd
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-10-27 18:51:29 +0530

    separate bin and lib

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#![allow(dead_code)]
+pub mod asm8085;
+pub mod cpu8085;
+pub mod ioport;
+pub mod memory;
+
+cfg_if::cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
