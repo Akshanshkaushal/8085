7557417ff619a13707b9af7782b22df5d90cb3e6
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-10-27 19:35:59 +0530

    updated footer header

--- www/src/components/footer.tsx
+++ www/src/components/footer.tsx
@@ -1,7 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub, faTwitter} from "@fortawesome/free-brands-svg-icons";
-import { Container, Box, Paper } from "@mui/material";
-import {ReactComponent as ParthLogo} from "../images/parth.svg"
+import { Container, Box, Paper } from "@mui/material"; 
 
 function Footer (props: {dark: boolean}) {
     const {dark} = props;
@@ -9,36 +8,7 @@ function Footer (props: {dark: boolean}) {
     return (
         <Container component={Paper} elevation={0} sx={{width:"50%"}}>
             <Box display="flex" alignContent="center" justifyContent="space-between">
-                <Box order={1} display="flex">
-                    <Box marginY="auto" component="div">
-                        Made with <span style={{color: "red"}}>♥</span> by
-                    </Box>
-                    <Box>
-                        <a href="https://parthpant.github.io" style={{textDecoration: "none", color:"inherit"}}>
-                            <ParthLogo height="6em" width="auto" style={{padding:0, margin:0}} fill={dark?"white":"black"}/>
-                        </a>
-                    </Box>
-                </Box>
-
-                {/* <Divider orientation="vertical" flexItem variant="middle"/> */}
-
-                <Box order={3} display="flex">
-                    <Box marginX={1} component="div" marginY="auto">
-                        <a href="https://github.com/ParthPant/PP8085" style={{textDecoration: "none", color:"inherit"}}>
-                        <FontAwesomeIcon icon={faGithub} size="2x"/>
-                        </a>
-                    </Box>
-                    <Box marginX={1} component="div" marginY="auto">
-                        <a href="https://www.linkedin.com/in/parth-pant-866bb4189/" style={{textDecoration: "none", color:"inherit"}}>
-                        <FontAwesomeIcon icon={faLinkedin} size="2x"/>
-                        </a>
-                    </Box>
-                    <Box marginX={1} component="div" marginY="auto">
-                        <a href="https://twitter.com/PantParth" style={{textDecoration: "none", color:"inherit"}}>
-                        <FontAwesomeIcon icon={faTwitter} size="2x"/>
-                        </a>
-                    </Box>
-               </Box>
+               
             </Box>
         </Container>
     )
--- www/src/components/header.tsx
+++ www/src/components/header.tsx
@@ -1,13 +1,10 @@
-import { Box, Typography } from "@mui/material";
-import {ReactComponent as Logo} from "../images/8085.svg"
+import { Box, Typography } from "@mui/material"; 
 
 function Header (props: {dark: boolean}) {
     const {dark} = props;
     return (
         <Box sx={{p:0, m:0}} display="flex" justifyContent="start">
-            <Box marginY="auto" display="flex" flexDirection="column">
-                <Logo height="10em" width="auto" fill={dark?"white":"black"} stroke={dark?"white":"black"}/>
-            </Box>
+           
             <Box display="flex" flexDirection="column" marginY="auto">
                 <Typography variant="h5">Intel 8085<br/>Microporcessor Emulator</Typography>
                 <Typography variant="subtitle1"><a href="https://en.wikipedia.org/wiki/Intel_8085" style={{color: "inherit"}}>wikipedia</a></Typography>
