95c9d04d59d6d9bf79de30d5c859e9fc07f1d17a
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-10-27 21:09:37 +0530

    updated vite.config modules added

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -2,15 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
   build: {
     outDir: 'build',
-    target: 'esnext'
+    target: 'esnext',
+    rollupOptions: {
+      external: ['wbg'],
+    },
   },
   plugins: [
     react(),
     svgr(),
-    wasm()
+    wasm(),
+    topLevelAwait(),
   ],
+  optimizeDeps: {
+    exclude: ['pp8085'],
+  },
 })
