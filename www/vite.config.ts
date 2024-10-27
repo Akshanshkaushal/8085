4cbab6d04b7f1df5dc58de762d389912b6402849
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-10-27 21:32:32 +0530

    workflow vite changes

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
+import path from 'path'
 
 export default defineConfig({
   build: {
-    outDir: 'build',
+    outDir: path.resolve(__dirname, 'build'),
     target: 'esnext',
     rollupOptions: {
       external: ['wbg'],
